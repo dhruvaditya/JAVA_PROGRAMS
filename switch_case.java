1a0b17366966207a1beff0e041abb4b3bf65ebe5
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2022-09-23 16:05:02 +0530

    Create switch_case.java

---
+++ switch_case.java
@@ -0,0 +1,51 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+int month = 4;
+
+    switch (month) {
+
+      case 1:
+
+        System.out.println("January");
+
+        break;
+
+      case 2:
+
+        System.out.println("February");
+
+        break;
+
+      case 3:
+
+        System.out.println("March");
+
+        break;
+
+      case 4:
+
+        System.out.println("April");
+
+        break;
+
+      case 5:
+
+        System.out.println("May");
+
+        break;
+
+      case 6:
+
+        System.out.println("June");
+
+        break;
+
+     default: System.out.println("In next half");
+
+}
+
+  }
+
+}
